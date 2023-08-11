8f374a8e8701220544116e987fd57ec800e3252c
hasan cimcik <cimcikhasan06@gmail.com>
2023-08-11 11:40:25 +0300

    Initial Commit

---
+++ AnimeBook/AnimeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeBookApp.swift
+//  AnimeBook
+//
+//  Created by Hasan PC on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
